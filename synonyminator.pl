281792a5ce08c1907da1440bc069c78f4fce4542
databox <headprogrammingczar@gmail.com>
2012-09-02 14:47:54 -0400

    script and sample output

---
+++ synonyminator.pl
@@ -0,0 +1,40 @@
+#!perl -W
+
+use strict;
+use warnings;
+
+main();
+
+sub main {
+  while (<>) {
+    s/\b(\w+)\b/&synonyminate($1)/eg;
+    print;
+  }
+
+  print "\n";
+}
+
+sub synonyminate {
+  my ($word) = @_;
+  my (@lines, $line, @synonyms, $rand);
+
+  @lines = `aiksaurus $word`;
+
+  @synonyms = ();
+
+  if ($lines[0] eq "*** No synonyms known. ***\n") {
+    return $word;
+  }
+
+  foreach $line (@lines) {
+    chomp $line;
+    if ($line !~ /^===/ and $line ne '') {
+      @synonyms = (@synonyms, split(/, /, $line));
+    }
+  }
+
+  $rand = int(rand(scalar @synonyms));
+
+  return $synonyms[$rand];
+}
+
---
+++ synonymized-README.md
@@ -0,0 +1,6 @@
+synonyminator
+=============
+
+Converts words no matter what irregular synonyms a la mode a tall late lamented of principle.
+
+
